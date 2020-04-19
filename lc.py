85cb843744fad57d05cf3df9947b26a29d8796ea
VictorLCT952 <57416797+VictorLCT952@users.noreply.github.com>
2020-04-19 12:08:33 +0300

    Add files via upload

---
+++ lc.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '!lc')
+
+@client.event
+async def on_ready():
+   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="LumberCraft"))
+   print('LumberCraft is Ready')
+
+client.run('NzAxMzUyMTcxODExMTEwOTIy.XpwPIQ.PKzBBi7EnpzdwODHs-ipCDvyIWk')
